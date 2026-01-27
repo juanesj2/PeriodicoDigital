8ddce04f26db201df6a57620c23ac4ce435dd0e1
juanesj2 <juanstivenaa82@gmail.com>
2026-01-27 14:00:00 +0100

    Subiendo plantilla inicial y app andrioid

--- proyecto/src/app/tabs/tabs.routes.ts
+++ proyecto/src/app/tabs/tabs.routes.ts
@@ -22,6 +22,16 @@ export const routes: Routes = [
           import('../tab3/tab3.page').then((m) => m.Tab3Page),
       },
       {
+        path: 'tab4',
+        loadComponent: () =>
+          import('../tab4/tab4.page').then((m) => m.Tab4Page),
+      },
+      {
+        path: 'tabMIAU',
+        loadComponent: () =>
+          import('../tabtabMIAU/tabtabMIAU.page').then((m) => m.TabtabMIAUPage),
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full',
