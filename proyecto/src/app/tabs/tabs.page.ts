787f13d5ecde79143c63f6d88a7e0f1744c9132e
AngelAlcantud <angelalcantud94@gmail.com>
2026-02-12 12:48:26 +0100

    Estado ahora de mi rama angel

--- proyecto/src/app/tabs/tabs.page.ts
+++ proyecto/src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonHeader, IonToolbar, IonButtons, IonButton, IonTitle, IonModal, IonContent, IonList, IonItem } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { homeOutline, hardwareChipOutline, footballOutline, briefcaseOutline, videocamOutline, sunnyOutline, moonOutline, newspaperOutline, bookmarkOutline, searchOutline, closeOutline, checkmarkCircleOutline } from 'ionicons/icons';
+import { homeOutline, hardwareChipOutline, footballOutline, briefcaseOutline, videocamOutline, sunnyOutline, moonOutline, newspaperOutline, bookmarkOutline, searchOutline, closeOutline, checkmarkCircleOutline, pricetagOutline } from 'ionicons/icons';
 import { CommonModule } from '@angular/common';
 import { RouterLink, Router } from '@angular/router';
 
@@ -14,10 +14,11 @@ import { RouterLink, Router } from '@angular/router';
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
-  
+
   // Variable para controlar el estado del modo oscuro (Dark Mode)
   esModoOscuro = false;
-  
+  colorActual: 'blue' | 'red' | 'green' | 'purple' = 'blue';
+
   // Estado del modal de categorías
   isModalOpen = false;
 
@@ -36,12 +37,12 @@ export class TabsPage {
 
   constructor(private router: Router) {
     // Registramos los iconos que utilizaremos en la interfaz
-    addIcons({ homeOutline, hardwareChipOutline, footballOutline, briefcaseOutline, videocamOutline, sunnyOutline, moonOutline, newspaperOutline, bookmarkOutline, searchOutline, closeOutline, checkmarkCircleOutline });
-    
+    addIcons({newspaperOutline,hardwareChipOutline,footballOutline,searchOutline,briefcaseOutline,videocamOutline,closeOutline,pricetagOutline,homeOutline,sunnyOutline,moonOutline,bookmarkOutline,checkmarkCircleOutline});
+
     // Detectamos la preferencia de color del sistema (claro u oscuro)
     const prefiereOscuro = window.matchMedia('(prefers-color-scheme: dark)');
     this.esModoOscuro = prefiereOscuro.matches;
-    this.actualizarTema(); 
+    this.actualizarTema();
 
     // Escuchamos cambios en la preferencia del sistema mientras la app está abierta
     prefiereOscuro.addEventListener('change', (mediaQuery) => {
@@ -77,4 +78,19 @@ export class TabsPage {
     document.body.classList.toggle('dark', this.esModoOscuro);
     document.body.classList.toggle('light', !this.esModoOscuro);
   }
+ cambiarColor(color: 'blue' | 'red' | 'green' | 'purple') {
+  this.colorActual = color;
+  this.actualizarColor();
+}
+
+private actualizarColor() {
+  document.body.classList.remove(
+    'theme-blue',
+    'theme-red',
+    'theme-green',
+    'theme-purple'
+  );
+
+  document.body.classList.add(`theme-${this.colorActual}`);
+}
 }
