4981cc01ecad4841b7903f3c0c092f5cc88aa3d9
juanesj2 <juanstivenaa82@gmail.com>
2026-02-25 08:58:31 +0100

    wip

--- proyecto/src/environments/environment.ts
+++ proyecto/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  // Key de juanstivenaa83@gmail.com
+  // Key 1
   //apiKey: 'f7ccc3a3c6b46baf70c05cf885ec4357'
 
-  // Key de juanstivenaa82@gmail.com
+  // Key 2
   apiKey: 'e4f6c616a73aefab9a334c62b7d7a9d6'
 };
